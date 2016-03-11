6c4a412dc4c83d0bfd03b7d1a494e745e8ce52ae
schaui6 <sanderfer.chau@gmail.com>
2016-03-11 14:14:49 -0800

    Update Questions.rb that included Jeffs

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,3 +1,29 @@
+get '/questions/new' do
+  #return a from for creating a new question
+  if request.xhr?
+  erb :'questions/_create_question_form', layout: false
+  else
+    erb :'questions/new'
+  end
+end
+
+post '/questions' do
+  #create a new question
+  @question = Question.new(params[:question])
+   if @question.save? #validation succeeded
+    if request.xhr?
+      erb :'show', locals: {question: @question}, layout: false
+    else
+      #not sure exactly where we want to redirect if not xhr
+      redirect "/question/#{@question.id}"
+    end
+   else
+    #validations failed. show the error to the user
+    @errors = @question.errors.full_message
+    erb :'questions/new'
+   end
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @q_comments = @question.comments
