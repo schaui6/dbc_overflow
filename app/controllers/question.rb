d2a4b134947666502b799713387d5777192b894f
Jeff Schneider <email": null>
2016-03-11 13:05:06 -0800

    Complete new question form, view, and controller

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -4,14 +4,28 @@
 
 get '/questions/new' do
   #return a from for creating a new question
-
-  erb :'questions/new'
+  if request.xhr?
+  erb :'questions/_create_question_form', layout: false
+  else
+    erb :'questions/new'
+  end
 end
 
 post '/questions' do
   #create a new question
-
-  @question = Question.create()
+  @question = Question.new(params[:question])
+   if @question.save? #validation succeeded
+    if request.xhr?
+      erb :'show', locals: {question: @question}, layout: false
+    else
+      #not sure exactly where we want to redirect if not xhr
+      redirect "/question/#{@question.id}"
+    end
+   else
+    #validations failed. show the error to the user
+    @errors = @chair.errors.full_message
+    erb :'questions/new'
+   end
 end
 
 # get '/question/:id' do
