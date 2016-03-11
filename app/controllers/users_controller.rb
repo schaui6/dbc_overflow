2d341e99d248b2c39e0d30e0d6a6d28b521de5f9
Aaron1515 <aaron1515@gmail.com>
2016-03-11 11:21:16 -0800

    aded previous merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,22 @@
 get '/users/:id' do
-  if session[:id]
+  # if session[:id]
     @user = User.find_by(id: params[:id])
     @questions = Question.where(poster_id: params[:id])
     @answers = Answer.where(responder_id: params[:id])
     erb :users
-  else
-    erb :index
-  end
+  # else
+    # erb :index
+  # end
 end
 
+
+# To be used for future features for hover effect
 get 'quick_question' do
 
   erb :_quick_question
 end
 
+# To be used for future features for hover effect
 get 'quick_answer' do
 
   erb :_quick_answer
